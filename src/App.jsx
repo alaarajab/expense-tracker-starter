f6a2873dc0e819edc3b4bd1462dc82eed971dd41
Alaa Rajab <alaa88rajab@gmail.com>
2026-05-06 13:21:44 -0500

    Add the ability to Delete transaction

--- src/App.jsx
+++ src/App.jsx
@@ -20,6 +20,11 @@ function App() {
     setTransactions([...transactions, transaction]);
   };
 
+  const handleDelete = (id) => {
+    if (!window.confirm("Are you sure you want to delete this transaction?")) return;
+    setTransactions(transactions.filter(t => t.id !== id));
+  };
+
   return (
     <div className="app">
       <h1>Finance Tracker</h1>
@@ -27,7 +32,7 @@ function App() {
 
       <Summary transactions={transactions} />
       <TransactionForm onAdd={handleAdd} />
-      <TransactionList transactions={transactions} />
+      <TransactionList transactions={transactions} onDelete={handleDelete} />
     </div>
   );
 }
